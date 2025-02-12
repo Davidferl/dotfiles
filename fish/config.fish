d3c5512eef8e28156a68c05b135b02cd6d1a4730
David Ferland <david.ferland.swe@gmail.com>
2025-02-12 17:23:29 -0500

    fix asdf

--- fish/config.fish
+++ fish/config.fish
@@ -4,5 +4,17 @@ end
 
 starship init fish | source
 
-source /opt/asdf-vm/asdf.fish
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 . ~/.asdf/plugins/java/set-java-home.fish
---
+++ fish/functions/ves.fish
@@ -0,0 +1,3 @@
+function ves
+source .venv/bin/activate.fish
+end
