77dd5de5992c54e6a85e0ab545c9e46c286e3190
David Ferland <david.ferland.swe@gmail.com>
2025-04-05 14:11:05 -0400

    update conf

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,5 +1,5 @@
 return {
-  "abhagwan/fzf-lua",
+  "ibhagwan/fzf-lua",
   keys = {
     { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (Root Dir)" },
     { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (Root Dir)" },
