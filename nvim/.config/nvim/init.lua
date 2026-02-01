9197bb120a11a2108040f559e7f9970a34b5d89f
David Ferland <david.ferland.swe@gmail.com>
2026-02-01 12:41:11 -0500

    simplify fish setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,3 +4,17 @@ vim.o.wrap = false
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
+vim.o.smartcase = true
+vim.o.ignorecase = true
+
+vim.o.hlsearch = false
+vim.o.signcolumn = 'yes'
+
+vim.cmd.colorscheme('retrobox')
+
+vim.keymap.set({'n', 'x'}, 'y', '"+y', {desc = 'Copy to clipboard'})
+vim.keymap.set({'n', 'x'}, 'p', '"+p', {desc = 'Paste clipboard content'})
+
+vim.g.mapleader = vim.keycode('<Space>')
+
+vim.keymap.set('n', '<leader>h', '<cmd>echo "hello there"<cr>')
