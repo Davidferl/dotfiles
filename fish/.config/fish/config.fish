2a7faa9cf452eeb5d90b6a023505a5393322bffb
David Ferland <david.ferland.swe@gmail.com>
2026-04-03 21:30:52 -0400

    up

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,8 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-starship init fish | source
 mise activate fish | source
+starship init fish | source
+zoxide init fish | source
 
 # PNPM config
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,18 +3,136 @@ vim.o.wrap = false
 
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
+vim.o.clipboard = 'unnamedplus'
 
 vim.o.smartcase = true
 vim.o.ignorecase = true
 
-vim.o.hlsearch = false
+vim.o.hlsearch = true
 vim.o.signcolumn = 'yes'
 
-vim.cmd.colorscheme('retrobox')
+vim.o.winborder = 'rounded'
+vim.o.background = "light"
+vim.cmd.colorscheme('default')
 
-vim.keymap.set({'n', 'x'}, 'y', '"+y', {desc = 'Copy to clipboard'})
-vim.keymap.set({'n', 'x'}, 'p', '"+p', {desc = 'Paste clipboard content'})
+vim.g.mapleader = vim.keycode('<space>')
 
-vim.g.mapleader = vim.keycode('<Space>')
+vim.pack.add({
+	'https://github.com/nvim-lua/plenary.nvim',
+	'https://github.com/nvim-treesitter/nvim-treesitter',
+	'https://github.com/neovim/nvim-lspconfig',
+	'https://github.com/nvim-telescope/telescope.nvim',
+	'https://github.com/kdheepak/lazygit.nvim',
+	'https://github.com/lewis6991/gitsigns.nvim',
+	'https://github.com/rrethy/vim-illuminate'
+})
 
-vim.keymap.set('n', '<leader>h', '<cmd>echo "hello there"<cr>')
+-- LSP
+
+vim.lsp.enable({ 'lua_ls'})
+vim.lsp.enable({ 'ts_ls'})
+vim.lsp.enable({ 'prismals' })
+vim.lsp.enable({ 'oxfmt' })
+vim.lsp.enable({ 'oxlint' })
+vim.lsp.enable({ 'tilt_ls' })
+
+-- Git
+
+vim.g.lazygit_floating_window_scaling_factor = 1
+
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+    local gitsigns = require('gitsigns')
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({']c', bang = true})
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end)
+
+    map('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({'[c', bang = true})
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end)
+
+    -- Actions
+    map('n', '<leader>hs', gitsigns.stage_hunk)
+    map('n', '<leader>hr', gitsigns.reset_hunk)
+
+    map('v', '<leader>hs', function()
+      gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+
+    map('v', '<leader>hr', function()
+      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+
+    map('n', '<leader>hS', gitsigns.stage_buffer)
+    map('n', '<leader>hR', gitsigns.reset_buffer)
+    map('n', '<leader>hp', gitsigns.preview_hunk)
+    map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+
+    map('n', '<leader>hb', function()
+      gitsigns.blame_line({ full = true })
+    end)
+
+    map('n', '<leader>hd', gitsigns.diffthis)
+
+    map('n', '<leader>hD', function()
+      gitsigns.diffthis('~')
+    end)
+
+    map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
+    map('n', '<leader>hq', gitsigns.setqflist)
+
+    -- Toggles
+    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+    map('n', '<leader>tw', gitsigns.toggle_word_diff)
+
+    -- Text object
+    map({'o', 'x'}, 'ih', gitsigns.select_hunk)
+  end
+}
+
+-- Keymaps
+
+vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = 'Telescope help tags' })
+
+vim.keymap.set('n', '<leader>g', vim.cmd.LazyGit, { desc = 'Open Lazygit'})
+
+
+
+-- vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+--   group = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true }),
+--   callback = function()
+--     vim.lsp.buf.document_highlight()
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+--   group = "lsp_document_highlight",
+--   callback = function()
+--     vim.lsp.buf.clear_references()
+--   end,
+-- })
